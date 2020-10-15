a94828e41ebb010e9a7b6e69e2d02971e7376cc4
Diarrah <diarrahcasiano@gmail.com>
2020-10-15 03:45:49 -0700

    Grouping for easy import

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from './Header';
+export { default as SearchBar } from './SearchBar';
+export { default as JobBoard } from './JobBoard';
+export { default as JobThumbnail } from './JobThumbnail';
+export { default as  GHSpinner } from './Spinner--GH';
+export { default as MagnifyingGlass } from './Spinner--glass';;
+export { default as Error } from './Error';
