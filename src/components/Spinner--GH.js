6e161780ad699983fb79788762c92115ade445a8
Diarrah <diarrahcasiano@gmail.com>
2020-11-26 13:34:33 -0800

    Image for GH spinner

--- src/components/Spinner--GH.js
+++ src/components/Spinner--GH.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { JobsContext } from '../App';
+import spinner_black from '../images/github-black.svg';
+import spinner_purple from '../images/github-purple.svg';
 
-const GHSpinner = ({ withinJobBoard }) => (
-    <div className={`spinner--gh ${withinJobBoard ? 'resize' : ''}`}>
-        <i className="fab fa-github" aria-hidden="true" />
-    </div>
-)
+const GHSpinner = ({ withinJobBoard }) => {
+    const { lightTheme } = useContext(JobsContext);
+    return (
+        <div className={`spinner--gh ${withinJobBoard ? 'resize' : ''}`}>
+            <img src={`${lightTheme ? spinner_black : spinner_purple}`} alt="Github logo as loading spinner" />
+        </div>
+    )
+}
 
 export default GHSpinner
